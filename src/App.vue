0d3049fcb7cd94534d97d2ac8d118d1b06a2ebe7
pyratt <pyratt@pm.me>
2024-03-26 14:21:01 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import clippers from './components/clippers.vue';
+defineProps(
+  {
+    msg: {
+    type: String,
+    default: 'pyratt'
+  }
+})
+</script>
+
+<template>
+  <div id="container">
+
+    <div id="left">
+      <h1>pyratt</h1>
+    </div>
+
+  <div id="right">
+    <div id="clippers">
+      <clippers src="@/../clippers/twitter/index.html" link="https://twitter.com/pyratt_" height='1000' width='600'  transform="rotate(-0.01turn) scale(0.8)" />
+      <clippers src="@/../clippers/mastodon/index.html" link="https://pyratt.social" height='400' width='700' transform="rotate(-0.02turn) scale(0.8)" left="300px"/>
+      <clippers src="@/../clippers/twitch/index.html"  link="https://lakave.live/tv" height='500' width='800' transform="rotate(0.03turn) scale(0.8)" left="280px"/>
+    </div>
+    
+  </div>
+
+</div>
+</template>
+
+<style>
+@font-face {
+  font-family: "SketchG";
+  src:
+    url("./assets/fonts/titles/sketch_gothic_school/Sketch Gothic School.ttf") format("truetype");
+}
+
+:root {
+  --dark: #202124;
+  --accent: #496E3A;
+}
+
+body {
+  background-repeat: repeat;
+  background-size: contain;
+  overflow: hidden;
+}
+</style>
+<style scoped>
+#container {
+  width:100vw;
+  height: 100vh;
+  background: url('./assets/bg.jpeg');
+  top: 0;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#left {
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 38vw;
+  height: 100vh;
+}
+#right {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+h1 {
+  color: var(--accent);
+  font-size: 10rem;
+  font-family: 'SketchG', serif;
+  margin: 0;
+  line-height: 14rem;
+}
+#clippers {
+  transform: scale(0.8);
+}
+#bg {
+  position: absolute;
+  top: -50px;
+}
+#bg {
+  display: inline-block;
+}
+</style>
---
+++ src/components/clippers.vue
@@ -0,0 +1,148 @@
+<script setup lang="ts">
+defineProps(
+  {
+    src: {
+      type: String,
+      default: '',
+      required: true
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    height: {
+      type: String,
+      default: '',
+      required: true
+    },
+    width: {
+      type: String,
+      default: '',
+      required: true
+    },
+    scrolling: {
+      type: String,
+      default: '',
+    },
+    transform: {
+      type: String,
+      default: '',
+    },
+    link: {
+      type: String,
+      default: '',
+    },
+    left: {
+      type: String,
+      default: '0'
+    }
+  }
+)
+</script>
+
+<template>
+  <router-link class="hover" to="link" :style="{
+      width: parseInt(width) - 19 + 'px',
+      height: parseInt(height) + 'px',
+      transform: transform,
+      left: left
+      }">
+    <div class='container'>
+
+        <iframe 
+        :src='src'
+          :title='title'
+          :height="height"
+          :width="width"
+          :scrolling="scrolling"
+        ></iframe>
+      <div class="background"></div>
+      </div>
+    </router-link>
+
+</template>
+
+<style scoped>
+router-link {
+  display: inline-block;
+}
+.container {
+  overflow: hidden;
+  border: solid 8px var(--dark);
+  border-radius: 16px;
+  padding: 0;
+  background-color: var(--dark);
+  z-index: 0;
+  cursor: pointer;
+  position: relative;
+  display: inline-block;
+}
+
+.hover {
+  cursor: pointer;
+  z-index: -1;
+  transform: scale(1);
+  background-color: transparent;
+  transition: transform 1s, z-index 0.3s step-start;
+  position: relative;
+}
+
+.hover::after,
+.container::after {
+  display: inline-block;
+}
+
+.hover:hover {
+  z-index: 1;
+  transform: scale(1);
+  transition: transform 1s, z-index 0.3s step-start;
+}
+
+.hover > .container > .background {
+  background-color: transparent;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+}
+
+.hover:hover > .container > .background {
+  transition: background-color 0.3s, transform 1s, z-index 0.3s step-start;
+  animation: unblack 1.2s;
+}
+
+@keyframes unblack {
+  from {
+    background-color: var(--dark);
+  }
+  to {
+    background-color: transparent;
+  }
+}
+
+
+
+iframe {
+  position: relative;
+  top: -2px;
+  left: -2px;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  background-color: powderblue;
+}
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+</style>
