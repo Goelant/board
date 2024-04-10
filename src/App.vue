37f53e6300b699441d4a39c75213e53fa6587d33
Dimitri <dimitri@nextdecision.fr>
2024-04-10 15:28:55 +0200

    J'ai fait une exponentielle mais je sais pas comment

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, onUpdated } from 'vue';
 defineProps(
   {
     msg: {
@@ -9,14 +9,13 @@ defineProps(
 })
 
 onMounted(() => {
-  console.log('coucou')
-  let h1 = document.querySelectorAll('h1');
-  h1.forEach((element:HTMLElement, index) => {
-      console.log(index)
-      element.style.top=90*index + 'px'
-  })
-})
-
+  console.log('updated')
+  if (window.innerWidth < 650) {
+    console.log(1-(0.2 + Math.exp(parseFloat(`0.`+window.innerWidth))))
+    let top = document.getElementById('top');
+    top ? top.style.scale = `${1-(1.9 - Math.exp(parseFloat(`0.`+window.innerWidth)))}` : null;
+  }
+});
 </script>
 
 <template>
@@ -24,10 +23,11 @@ onMounted(() => {
 
     <div id="top">
       <div id="top-container">
-        <h1 style='z-index: 0;'>pyratt</h1>
-        <h1 style='z-index: 2;'>pyratt</h1>
-        <h1 style='z-index: 1;'>pyratt</h1>
+        <h1 style='z-index: 1; top: 0px;'>pyratt</h1>
+        <h1 style='z-index: 3; top: 90px;'>pyratt</h1>
+        <h1 style='z-index: 2; top: 180px;'>pyratt</h1>
       </div>
+      <img id='grid-image' src="./assets/grid.png" />
     </div>
 
   <div id="right">
@@ -78,12 +78,47 @@ body {
   border-radius: 16px;
   border: 4px solid rgb(6, 28, 1);
   overflow: hidden;
-  box-shadow: 6px 6px rgb(6, 28, 1), 10px 10px rgb(6, 28, 1);
+  box-shadow: 5px 5px rgb(6, 28, 1), 8px 8px rgb(6, 28, 1);
+  cursor: pointer;
+  transition: ease-out 0.2s;
+  transform: scale(.8);
+}
+
+#top:hover {
+  border: 4px solid rgb(6, 28, 1);
+  box-shadow: 8px 8px rgb(6, 28, 1), 12px 12px rgb(6, 28, 1);
+  transition: ease-out 0.2s;
+}
+
+#top h1,
+#top #grid-image,
+#top:hover h1 {
+  transition: ease-out 0.2s;
+  left: 0;
+}
+
+#top:hover h1:first-child {
+  left: -250px !important;
+}
+#top:hover h1:last-child {
+  left: 250px !important;
+}
+#top:hover #grid-image {
+  transition: ease-out 0.2s;
+  opacity: .75;
 }
 
 #top-container {
   position: relative;
   top: -32px;
+}
+
+#grid-image {
+  width: 115%;
+  transform: rotate(25deg) translateX(-8%) translateY(-20%);
+  z-index: 0;
+  opacity: .5;
+  transition: ease-out 0.2s;
 }
 
 h1 {
@@ -95,4 +130,5 @@ h1 {
   line-height: 7rem;
   text-shadow: 2px 2px 1px rgb(238, 230, 201), -2px -2px 1px white;
 }
+
 </style>
